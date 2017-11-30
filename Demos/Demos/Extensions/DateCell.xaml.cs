f2140bb6fa8ffbd45d91e1188275270e142414c0
Edgar Gonzalez <edgargonzalez.developer@gmail.com>
2017-11-30 21:44:51 +0100

    Table View has been implemented

---
+++ Demos/Demos/Extensions/DateCell.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demos.Extensions
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DateCell : ViewCell
+	{
+
+        //This is the way to bindable properties
+        public static readonly BindableProperty LabelProperty =
+            BindableProperty.Create("Label", typeof(string) , typeof(DateCell));
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public DateCell ()
+		{
+			InitializeComponent ();
+            BindingContext = this;
+		}
+	}
+}
