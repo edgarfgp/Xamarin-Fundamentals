01cacf438f2b8cb7b7cf7d417490c4f91c01bced
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@hotmail.com>
2017-12-27 12:10:47 +0100

    FileSystem on Droid Demo has been removed

---
+++ Demos/Demos.iOS/Persistence/SQLiteDb.cs
@@ -0,0 +1,10 @@
+﻿using Demos.iOS.Persistence;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+namespace Demos.iOS.Persistence
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+    }
+}
